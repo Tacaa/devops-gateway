1fb7136bf3d2cda37bfdc8a1599634258908c995
Tatjana Gavrilović <tatjana.gavrilovic@openfyle.com>
2024-12-06 16:37:37 +0100

    init: Gateway Service set up

---
+++ src/main/java/com/devops/devops_gateway/DevopsGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.devops.devops_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devops/devops_gateway/DevopsGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.devops_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
